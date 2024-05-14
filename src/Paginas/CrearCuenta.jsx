38f34611e226ef03919064d793b203271457bce7
Juan Pablo Hurtado Agudelo <123783172+juanpabloh18@users.noreply.github.com>
2024-05-14 00:47:11 -0500

    Ya hay admin

--- src/Paginas/CrearCuenta.jsx
+++ src/Paginas/CrearCuenta.jsx
@@ -1,18 +1,22 @@
-import React, { useState,useEffect } from "react";
+import React, { useState } from "react";
 import { auth } from "../bd/firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 
 
 const CrearCuenta = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] =useState("");
-    
+    const navigate = useNavigate();
 
     const SignUp = (e) =>{
         e.preventDefault();
         createUserWithEmailAndPassword(auth,email,password).then ((useCredential)=>{
-            console.log(useCredential)
+            console.log(useCredential);
+                navigate('/');
         }).catch((error)=>{
             console.log(error);
         })
@@ -25,10 +29,11 @@ const CrearCuenta = () => {
         <>
            <div>
             <form onSubmit={SignUp}>
-                <h1>CrearCuenta|</h1>
+                <h1>CrearCuenta</h1>
                 <input type="email" placeholder="Correo" value={email} onChange={(e) =>setEmail(e.target.value)}></input>
                 <input type="password" placeholder="contraseña" value={password} onChange={(e) => setPassword(e.target.value)}></input>
               <button type="submit">Sign up</button>
+              <p>Si ya tienes cuenta has<Link to="/">Login</Link></p>
             </form>
            </div>
         </>
--- src/Paginas/Login.jsx
+++ src/Paginas/Login.jsx
@@ -2,23 +2,42 @@ import React, { useState,useEffect } from "react";
 import { auth } from "../bd/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
+import { Link, useNavigate } from "react-router-dom";
+
 
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] =useState("");
+    const [showError, setShowError] = useState(false);
+    const [loginError, setLoginError] = useState("");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();
     
 
-    const SignIn = (e) =>{
+    const SignIn = (e) => {
         e.preventDefault();
-        signInWithEmailAndPassword(auth,email,password).then ((useCredential)=>{
-            console.log(useCredential)
-        }).catch((error)=>{
-            console.log(error);
-        })
+        if (email === "juan@18.com" && password === "181001") {
+            setIsLoggedIn(true);
+            navigate('/Home');
+        } else {
+            signInWithEmailAndPassword(auth, email, password)
+                .then((useCredential) => {
+                    console.log(useCredential);
+                    setIsLoggedIn(true);
+                    navigate('/Home');
+                })
+                .catch((error) => {
+                    console.log(error);
+                    setLoginError("Las credenciales no coinciden. Por favor, inténtalo de nuevo.");
+                    setShowError(true); // Mostrar el mensaje de error
+                    setTimeout(() => {
+                        setShowError(false); // Ocultar el mensaje de error después de 5 segundos
+                    }, 5000);
+                });
+        }
+    };
 
-
-    }
     
 
     return (
@@ -29,6 +48,8 @@ const Login = () => {
                 <input type="email" placeholder="Correo" value={email} onChange={(e) =>setEmail(e.target.value)}></input>
                 <input type="password" placeholder="contraseña" value={password} onChange={(e) => setPassword(e.target.value)}></input>
               <button type="submit">Login</button>
+              <p>No tienes cuenta <Link to="/CrearCuenta">Registrate</Link></p>
+              {showError && <p style={{ textAlign: "center", color: "red" }}>{loginError}</p>}
             </form>
            </div>
         </>
