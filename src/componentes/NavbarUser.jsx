aa19dbb0a33de6d5810d484d951a7f3644fd2f5b
Juan Pablo Hurtado Agudelo <123783172+juanpabloh18@users.noreply.github.com>
2024-05-20 12:03:00 -0500

    Botón cerrar sesión para usuarios

--- src/componentes/NavbarUser.jsx
+++ src/componentes/NavbarUser.jsx
@@ -2,10 +2,23 @@ import React from "react";
 import { Menu, Container, Button,Image } from "semantic-ui-react";
 import { useNavigate, Link } from "react-router-dom";
 import reactLogo from '../assets/react.svg'
-
+import { auth } from "../bd/firebase";
+import { signOut } from "firebase/auth";
 
 const NavbarUser =() =>{
     const navigate = useNavigate();
+
+    const handleLogout = () => {
+        signOut(auth)
+          .then(() => {
+            navigate('/');
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      };
+
+
     return(
         <Menu inverted borderless style ={{padding: "0.3rem", marginBotton:"20px"}} attached>
             <Container>
@@ -19,7 +32,9 @@ const NavbarUser =() =>{
                 </Menu.Item>
                 <Menu.Item position="right">
                     <Button size="mini" primary onClick={()=> navigate("/Agregar")}>Mis torneos</Button>
-
+                </Menu.Item>
+                <Menu.Item>
+                    <Button size="mini" primary onClick={handleLogout} >Cerrar sesión</Button>
                 </Menu.Item>
             </Container>
         </Menu>
