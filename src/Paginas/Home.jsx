bb03506d7ed7ed96a4da70297801ff065e73e617
Juan Pablo Hurtado Agudelo <123783172+juanpabloh18@users.noreply.github.com>
2024-05-21 14:50:28 -0500

    se arreglo problema al recargar la VistaAdmin

--- src/Paginas/Home.jsx
+++ src/Paginas/Home.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
 import VistaAdmin from "../componentes/VistaAdmin";
 import VistaUser from "../componentes/VistaUser";
+import { auth } from "../bd/firebase";
 
+const Home = () => {
+  const [userRole, setUserRole] = useState("");
 
+  useEffect(() => {
+    
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        
+        setUserRole("user");
+      } else {
+       
+        setUserRole("");
+      }
+    });
 
+    
+    return () => unsubscribe();
+  }, []);
 
-const Home = ({ userRole }) => {
   return (
     <div>
-      {userRole === 'admin' ? <VistaAdmin /> : <VistaUser />}
+      {userRole === "admin" ? <VistaAdmin /> : <VistaUser />}
     </div>
   );
 }
-
 
 export default Home;
