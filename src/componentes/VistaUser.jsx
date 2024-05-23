226aadcd175bcb85e76008ec5a6a7bdde2f42d81
Juan Pablo Hurtado Agudelo <123783172+juanpabloh18@users.noreply.github.com>
2024-05-23 11:31:46 -0500

    Se uso sweet alert para la inscripción

--- src/componentes/VistaUser.jsx
+++ src/componentes/VistaUser.jsx
@@ -4,7 +4,7 @@ import { Button, Card, Grid, Container, Image } from "semantic-ui-react";
 import { collection, onSnapshot, addDoc, updateDoc, deleteDoc, doc, getDoc, getDocs, query, where, arrayUnion, arrayRemove } from "firebase/firestore";
 import NavbarUser from "./NavbarUser";
 import ModalComponente2 from "./ModalComponente2";
-
+import Swal from "sweetalert2";
 const VistaUser = () => {
   const [torneos, setTorneos] = useState([]);
   const [inscritos, setInscritos] = useState([]);
@@ -59,20 +59,20 @@ const VistaUser = () => {
       if (user) {
         const uid = user.uid;
 
-        // Obtener datos más recientes del torneo
+        
         const torneoRef = doc(db, "torneos", torneo.id);
         const torneoSnap = await getDoc(torneoRef);
         if (torneoSnap.exists()) {
           const torneoData = torneoSnap.data();
           const updatedParticipants = torneoData.participants - 1;
 
-          // Actualizar el número de participantes y agregar email a la lista de participantes
+          
           await updateDoc(torneoRef, {
             participants: updatedParticipants,
             participantes: arrayUnion(user.email),
           });
 
-          // Añadir la inscripción del usuario
+          
           await addDoc(collection(db, "users", uid, "torneosInscritos"), {
             torneoId: torneo.id,
             name: torneo.name,
@@ -82,9 +82,13 @@ const VistaUser = () => {
             img: torneo.img,
           });
 
-          // Actualizar estado local
+          
           setInscritos([...inscritos, torneo.id]);
-          alert("Te has inscrito en el torneo correctamente.");
+          Swal.fire({
+            icon: 'success',
+            title: 'Inscripción exitosa',
+            text: 'Te has inscrito en el torneo correctamente.',
+          });
         }
       }
     } catch (error) {
@@ -98,20 +102,20 @@ const VistaUser = () => {
       if (user) {
         const uid = user.uid;
 
-        // Obtener referencia del torneo
+        
         const torneoRef = doc(db, "torneos", torneo.id);
         const torneoSnap = await getDoc(torneoRef);
         if (torneoSnap.exists()) {
           const torneoData = torneoSnap.data();
           const updatedParticipants = torneoData.participants + 1;
 
-          // Actualizar el número de participantes y eliminar email de la lista de participantes
+          
           await updateDoc(torneoRef, {
             participants: updatedParticipants,
             participantes: arrayRemove(user.email),
           });
 
-          // Eliminar la inscripción del usuario
+          
           const q = query(
             collection(db, "users", uid, "torneosInscritos"),
             where("torneoId", "==", torneo.id)
@@ -121,9 +125,13 @@ const VistaUser = () => {
             await deleteDoc(inscripcion.ref);
           });
 
-          // Actualizar estado local
+        
           setInscritos(inscritos.filter((id) => id !== torneo.id));
-          alert("Has cancelado tu inscripción en el torneo.");
+          Swal.fire({
+            icon: 'success',
+            title: 'Cancelación exitosa',
+            text: 'Has cancelado tu inscripción al torneo.',
+          });
         }
       }
     } catch (error) {
